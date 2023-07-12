4a7c516a6d8f2c49fd264877da1774d2f9884010
h21alborzi <138998780+h21alborzi@users.noreply.github.com>
2023-07-12 20:57:24 +0330

    Add files via upload

--- Digikala/js/searchbarjs.js
+++ Digikala/js/searchbarjs.js
@@ -1,15 +1,3 @@
-const basicAutocomplete = document.querySelector('#search-autocomplete');
-const data = ['One', 'Two', 'Three', 'Four', 'Five'];
-const dataFilter = (value) => {
-    return data.filter((item) => {
-        return item.toLowerCase().startsWith(value.toLowerCase());
-    });
-};
-
-new mdb.Autocomplete(basicAutocomplete, {
-    filter: dataFilter
-});
-
 // ===========================================================================================
 
 let mybutton = document.getElementById("btn-back-to-top");
